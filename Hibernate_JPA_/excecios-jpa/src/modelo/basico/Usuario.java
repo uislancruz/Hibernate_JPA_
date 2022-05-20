f8909eaea4a22e7751f737aa92d3d8030c6bbf36
Uislan Cruz <uislansk8@hotmail.com>
2022-05-20 20:17:56 -0300

    Ajustes

--- Hibernate_JPA_/excecios-jpa/src/modelo/basico/Usuario.java
+++ Hibernate_JPA_/excecios-jpa/src/modelo/basico/Usuario.java
@@ -1,11 +1,14 @@
 package modelo.basico;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Usuario {
 	
+	@Id
 	private Long id;
+	
 	
 	private String nome;
 	
