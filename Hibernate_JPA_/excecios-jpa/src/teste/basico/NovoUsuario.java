3be9314321bbcd9943e8ccfdb924ce4cab58ba1d
Uislan Cruz <uislansk8@hotmail.com>
2022-05-25 00:09:23 -0300

    Update NovoUsuario.java

--- Hibernate_JPA_/excecios-jpa/src/teste/basico/NovoUsuario.java
+++ Hibernate_JPA_/excecios-jpa/src/teste/basico/NovoUsuario.java
@@ -4,12 +4,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import modelo.basico.Usuario;
+
 public class NovoUsuario {
 	public static void main(String[] args) {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("excecios-jpa");
 		EntityManager em = emf.createEntityManager();
 		
+		Usuario novoUsuario = new Usuario("Djalma", "djalma@lanche.com.br");
+		
+		em.persist(novoUsuario);
+		
+		em.close();
+		emf.close();
 	}
 
 }
