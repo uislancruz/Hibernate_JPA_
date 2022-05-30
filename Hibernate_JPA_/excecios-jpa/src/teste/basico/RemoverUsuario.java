a39af200ba01df8eecc9db5aef2060849f7f0ffe
Uislan Cruz <uislansk8@hotmail.com>
2022-05-30 20:26:06 -0300

    Update RemoverUsuario.java

--- Hibernate_JPA_/excecios-jpa/src/teste/basico/RemoverUsuario.java
+++ Hibernate_JPA_/excecios-jpa/src/teste/basico/RemoverUsuario.java
@@ -1,5 +1,29 @@
 package teste.basico;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
 public class RemoverUsuario {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("excecio-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Usuario usuario = em.find(Usuario.class, 8L);
+		
+		if(usuario != null) {
+			em.getTransaction().begin();
+			em.remove(usuario);
+			em.getTransaction().commit();
+		}
+		
+		em.close();
+		emf.close();
+		
+	}
 
 }
