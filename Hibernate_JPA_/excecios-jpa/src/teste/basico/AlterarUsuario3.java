de3583f37b2abd238cb8f9a30cfc69a5db909284
Uislan Cruz <uislansk8@hotmail.com>
2022-05-30 20:00:26 -0300

    Update AlterarUsuario3.java

--- Hibernate_JPA_/excecios-jpa/src/teste/basico/AlterarUsuario3.java
+++ Hibernate_JPA_/excecios-jpa/src/teste/basico/AlterarUsuario3.java
@@ -1,5 +1,26 @@
 package teste.basico;
 
-public class AlterarUsuario3 {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import modelo.basico.Usuario;
+
+public class AlterarUsuario3 {
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("excecios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Usuario usuario = em.find(Usuario.class, 7L);
+		em.detach(usuario);
+		usuario.setNome("Leonado Leitão");
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
 }
