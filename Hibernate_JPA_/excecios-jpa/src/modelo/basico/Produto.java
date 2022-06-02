bfa9e02bdb5b3e09cf2c467d8bbe4f80d0b10257
Uislan Cruz <uislansk8@hotmail.com>
2022-06-01 21:05:06 -0300

    Produtos
    Inserindo a tabela produtos no Banco

--- Hibernate_JPA_/excecios-jpa/src/modelo/basico/Produto.java
+++ Hibernate_JPA_/excecios-jpa/src/modelo/basico/Produto.java
@@ -1,21 +1,31 @@
 package modelo.basico;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "produtos")
 public class Produto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "prod_nome", length = 200, nullable = false)
 	private String nome;
+	
+	@Column(name = "prod_preco", nullable = false, precision = 11, scale = 2)
 	private Double preco;
 		
 	public Produto() {
 		
 	}
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	public Long getId() {
 		return id;
 	}
