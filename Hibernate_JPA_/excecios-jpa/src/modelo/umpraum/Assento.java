fd2faf7a0aa9f9ff4f5ba1e0feff0fea1a0aa933
Uislan Cruz <uislansk8@hotmail.com>
2022-06-13 20:50:48 -0300

    Update Assento.java
    Corrigindo o nome para name

--- Hibernate_JPA_/excecios-jpa/src/modelo/umpraum/Assento.java
+++ Hibernate_JPA_/excecios-jpa/src/modelo/umpraum/Assento.java
@@ -1,5 +1,21 @@
 package modelo.umpraum;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "assentos")
 public class Assento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+
 
 }
