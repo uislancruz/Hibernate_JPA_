0e25f0cb48dcaa4e2152d483cf515e3642503b36
Uislan Cruz <uislansk8@hotmail.com>
2022-06-13 21:46:32 -0300

    Update Cliente.java

--- Hibernate_JPA_/excecios-jpa/src/modelo/umpraum/Cliente.java
+++ Hibernate_JPA_/excecios-jpa/src/modelo/umpraum/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -18,6 +19,7 @@ public class Cliente {
 	private String nome;
 	
 	@OneToOne
+	@JoinColumn(name = "assento_id", unique = true)
 	private Assento assento;
 
 	public Cliente() {
