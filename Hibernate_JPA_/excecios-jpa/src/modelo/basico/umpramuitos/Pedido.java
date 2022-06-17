aedabafa048698b83d629bd13e84d0bb6936aaf7
Uislan Cruz <uislansk8@hotmail.com>
2022-06-17 12:56:38 -0300

    Update Pedido.java

--- Hibernate_JPA_/excecios-jpa/src/modelo/basico/umpramuitos/Pedido.java
+++ Hibernate_JPA_/excecios-jpa/src/modelo/basico/umpramuitos/Pedido.java
@@ -2,8 +2,15 @@ package modelo.basico.umpramuitos;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pedido {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private Date data;
