2ddcec182ca34de20bfee828dd8fd5d9c2b6f020
Uislan Cruz <uislansk8@hotmail.com>
2022-06-26 13:43:27 -0300

    Update ObterUsuario.java

--- Hibernate_JPA_/excecios-jpa/src/teste/basico/ObterUsuario.java
+++ Hibernate_JPA_/excecios-jpa/src/teste/basico/ObterUsuario.java
@@ -13,7 +13,7 @@ public class ObterUsuario {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("excecios-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Usuario usuario = em.find(Usuario.class, 1L);
+		Usuario usuario = em.find(Usuario.class, 7L);
 		System.out.println(usuario.getNome());
 		
 		em.close();
